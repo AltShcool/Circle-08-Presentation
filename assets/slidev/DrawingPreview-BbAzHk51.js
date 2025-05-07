f3624834174159c4c034aa4a2c1c8f5c557cc812
ChrisRoland <chrisebuberoland@gmail.com>
2025-05-07 20:17:00 +0100

    update slides

---
+++ assets/slidev/DrawingPreview-BbAzHk51.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-DwuTLOYe.js";import"../modules/vue-DnfkPNEi.js";import"../monaco/bundled-types-DammcoRR.js";import"../modules/file-saver-BzOOqXCn.js";import"../index-OtCFIimV.js";import"../modules/shiki-Cj2c-_Dm.js";export{o as default};
--- assets/slidev/DrawingPreview-Bz5rsH6B.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-R9awXGTu.js";import"../modules/vue-DnfkPNEi.js";import"../monaco/bundled-types-DammcoRR.js";import"../modules/file-saver-BzOOqXCn.js";import"../index-CcXEiHIT.js";import"../modules/shiki-Cj2c-_Dm.js";export{o as default};
