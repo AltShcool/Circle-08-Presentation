87e80d51dc51058279583c3bad65d2292c51e548
ChrisRoland <chrisebuberoland@gmail.com>
2025-05-07 19:17:51 +0100

    update slides

--- assets/slidev/default-BXgr93vG.js
+++
@@ -1,1 +0,0 @@
-import{f as o,o as t,j as s}from"../modules/vue-DnfkPNEi.js";import{_ as r}from"../index-DvDVnUD0.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
